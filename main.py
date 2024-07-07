96df02451cdf7934f7e15e66877d38088f87e452
Jared Hoyt <jared.v.hoyt@gmail.com>
2024-07-07 12:53:04 -0500

    Add argparse functionality and remove error calculations.

--- main.py
+++ main.py
@@ -1,58 +1,43 @@
+import argparse
 import numpy as np
-import matplotlib.pyplot as plt
-from services.error import calculate_error
-from services.interpolation import inverse_distance_weighting, shape_function
-
-
-def plot_data(data, title):
-    x = data[:, 0]
-    y = data[:, 1]
-    z = data[:, 2]
-
-    plt.figure()
-    plt.scatter(x, y, c=z, cmap="viridis")
-    plt.colorbar(label="Interpolated Value")
-    plt.title(title)
-    plt.xlabel("X Coordinate")
-    plt.ylabel("Y Coordinate")
-    plt.grid(True)
-    plt.show()
+import os
+from interpolation.methods import inverse_distance_weighting, shape_function
+from services.plot import plot_data
 
 
 def main():
-    data = np.genfromtxt("./data/data.csv", delimiter=",")
-
-    # Inverse Distance Weighting
-    interpolated_data = inverse_distance_weighting(data)
-    combined_array = np.vstack((interpolated_data, data))
-    np.savetxt(
-        "./data/interpolation/inverse_distance_weighting.csv",
-        np.unique(interpolated_data, axis=0),
-        delimiter=",",
-        fmt="%f"
+    parser = argparse.ArgumentParser(
+        description="A Python-based interpolation application that utilizes real-world point-cloud data and spatial interpolation techniques to create a smooth heightmap of a given area.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
+
+    parser.add_argument("method", type=str, choices=["inverse_distance_weighting", "shape_function"], help="The interpolation method used.")
+    parser.add_argument("points", type=str, help="The relative path to the CSV file containing the 3-dimensional point data.")
+    parser.add_argument("-p", "--power", type=int, required=False, default=2, help="The power parameter used in the Inverse Distance Weighting equation.")
+    parser.add_argument("-r", "--resolution", type=int, required=False, default=50, help="The number of subdivisions along each axis.")
+    parser.add_argument("-s", "--save", action="store_true", help="Save the data as a CSV file to the same directory where the 3-dimensional point data is stored.")
+
+    args = parser.parse_args()
+
+    points = np.genfromtxt(args.points, delimiter=",")
+
+    match args.method:
+        case "inverse_distance_weighting":
+            interpolated_data = inverse_distance_weighting(points, args.resolution, args.power)
+        case "shape_function":
+            interpolated_data = shape_function(points, args.resolution)
+
+    combined_array = np.vstack((interpolated_data, interpolated_data))
+
+    if args.save:
+        np.savetxt(
+            os.path.dirname(args.points) + "/interpolated_data.csv",
+            np.unique(interpolated_data, axis=0),
+            delimiter=",",
+            fmt="%f"
+        )
+
     plot_data(combined_array, "Inverse Distance Weighting")
-
-    mae, rmse, mse = calculate_error("./data/error/inverse_distance_weighting/actual_vs_calculated_rows.csv")
-    print(f"Inverse Distance Weighting MAE:  {mae:.4f}")
-    print(f"Inverse Distance Weighting RMSE: {rmse:.4f}")
-    print(f"Inverse Distance Weighting MSE:  {mse:.4f}")
-
-    # Shape Function Interpolation
-    interpolated_data = shape_function(data)
-    combined_array = np.vstack((interpolated_data, data))
-    np.savetxt(
-        "./data/interpolation/shape_function.csv",
-        np.unique(interpolated_data, axis=0),
-        delimiter=",",
-        fmt="%f"
-    )
-    plot_data(combined_array, "Shape Function Interpolation")
-
-    mae, rmse, mse = calculate_error("./data/error/shape_function/actual_vs_calculated_rows.csv")
-    print(f"Shape Function MAE:  {mae:.4f}")
-    print(f"Shape Function RMSE: {rmse:.4f}")
-    print(f"Shape Function MSE:  {mse:.4f}")
 
 
 if __name__ == "__main__":
